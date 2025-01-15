c04a0289a8f1d19273d2583440ff1d2bed32e518
Sergio Matamala <sergio.matamala@catena.cl>
2025-01-15 14:51:17 -0300

    :sparkles: add jacoco for code coverage

---
+++ src/test/java/cl/mineduc/sidep/sostenedorapi/SostenedorApiApplicationTest.java
@@ -0,0 +1,15 @@
+package cl.mineduc.sidep.sostenedorapi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class SostenedorApiApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
